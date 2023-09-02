29f9680f8271118b738e03be01cbdcd44453794b
Nanna <nanaa.gonzalez@gmail.com>
2023-09-02 18:34:03 +0200

    Add tech logos as components and style tecnologies section

--- src/views/Tecnologias/Tecnologias.jsx
+++ src/views/Tecnologias/Tecnologias.jsx
@@ -1,10 +1,10 @@
-import ListaTecnologias from './ListaTecnologias';
+import ListaTecnologias from './ListaTecnologias/ListaTecnologias';
 import './tecnologias.css';
 
 const Tecnologias = () => {
   return (
     <section id="Tecnologias" className='tecnologias'>
-      <h2>Tecnologias</h2>
+      <h2>TECNOLOGÍAS</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, id?</p>
       <ListaTecnologias/>
     </section>
