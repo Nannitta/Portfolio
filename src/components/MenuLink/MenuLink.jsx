2b4fd001a9247a9cf5af60f2a7526ad7597ff52a
Nanna <nanaa.gonzalez@gmail.com>
2023-09-08 17:51:21 +0200

    Fix nav menu color changing when passing through the technologies section

--- src/components/MenuLink/MenuLink.jsx
+++ src/components/MenuLink/MenuLink.jsx
@@ -1,9 +1,37 @@
 import PropTypes from 'prop-types';
 import { HashLink } from 'react-router-hash-link';
-
+import { useEffect, useState } from 'react';
 import './menulink.css';
 
 const MenuLink = ({ text, linkId }) => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    function handleScrollY () {
+      setScrollY(window.scrollY);
+      console.log(scrollY);      
+      const links = document.querySelectorAll('.section-links');
+      const [sobremi, , proyectos] = links;
+
+      if (scrollY > 400 && scrollY < 900) {
+        proyectos.classList.add('bg-links');
+        proyectos.style.transition = 'background-color 0.5s';
+      } else {
+        proyectos.classList.remove('bg-links');
+      }
+      
+      if (scrollY > 620 && scrollY < 1150) {
+        sobremi.classList.add('bg-links');
+        sobremi.style.transition = 'background-color 0.5s';        
+      } else{
+        sobremi.classList.remove('bg-links');        
+      }
+    }
+    window.addEventListener('scroll', handleScrollY);
+
+    return () => window.removeEventListener('scroll', handleScrollY);
+  }, [scrollY]);
+
   return <li className='section-links'><HashLink to={`/${linkId}`}>{text}</HashLink></li>;
 };
 
