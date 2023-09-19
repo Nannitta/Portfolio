cd1bbf186b651254342777f9e86283782ea9ecf4
Nanna <nanaa.gonzalez@gmail.com>
2023-09-19 20:03:22 +0200

    Add an animation to the project list when in the viewport

--- src/views/MyProjects/Projects.jsx
+++ src/views/MyProjects/Projects.jsx
@@ -1,12 +1,20 @@
 import './projects.css';
 import ProjectsList from './ProjectsList/ProjectsList';
 import MyProjects from '../../assets/proyectos.json';
+import { useInView } from 'react-intersection-observer';
 
 const Projects = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+    triggerOnce: true
+  });
+
+  const animation = inView ? 'animate__fadeInUp projects-list': 'projects-list';
+
   return (
     <section id="Proyectos" className='projects'>
-      <h2>PROYECTOS</h2>
-      <ProjectsList projects={MyProjects}/>
+      <h2 ref={ref}>PROYECTOS</h2>
+      <ProjectsList projects={MyProjects} animation={animation}/>
     </section>
   );
 };
